e1c37ab7a13b2a3d73673ea11cd9c5b05465c43c
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2026-02-20 20:41:11 -0500

    wip

--- source/funkin/objects/cutscenes/DialogueCutscene.hx
+++ source/funkin/objects/cutscenes/DialogueCutscene.hx
@@ -3,32 +3,33 @@ package funkin.objects.cutscenes;
 import flixel.addons.text.FlxTypeText;
 import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
+import textbox.*;
 
 typedef DialogueFile = {
 	var dialogue:Array<DialogueLine>;
 	var box_style:String;
 }
+
 typedef DialogueLine = {
 	var text:String;
 	var character:String;
 	var character_anim:String;
 	var text_speed:Float;
 	var text_size:Int; // custom font size
-	var line_sound:Array<String>; 
+	var line_sound:Array<String>;
 	var box_animation:String;
 }
 
-class DialogueCutscene extends Cutscene
-{
+class DialogueCutscene extends Cutscene {
 	/**
 	 * How long it should take until the dialogue first starts
 	 * Set to 0 for instant start time.
-	*/
+	 */
 	public var introDelay:Float = 2;
-	
+
 	/** Whether the player is able to progress the dialogue. **/
 	public var canProgressDialogue:Bool = false;
-	
+
 	/** Variable that allows you to keep all characters on screen if you want too. **/
 	public var keepAllCharactersOnScreen:Bool = false;
 
@@ -36,15 +37,15 @@ class DialogueCutscene extends Cutscene
 	var characters:Array<DialogueCharacter> = [];
 
 	public var curLine:Int = 0;
-	
+
 	var dialogueFile:DialogueFile;
-	var dialogueText:FlxTypeText;
+	var dialogueText:Textbox;
 	var box:DialogueBox;
 	var curCharacter:DialogueCharacter;
 
 	var finishedLine:Bool = false;
-	
-	public function new(dialoguePath:String)
+
+	public function new(dialoguePath:String) 
 	{
 		super();
 		onEnd.addOnce(endDialogue);
@@ -60,13 +61,40 @@ class DialogueCutscene extends Cutscene
 		add(box);
 
 		box.script?.call("onCreatePost");
-		
-		dialogueText = new FlxTypeText(box.textOffsets[0], box.textOffsets[1], box.textWidth, '', 32);
-		dialogueText.setFormat(Paths.font(box.font), 32, FlxColor.fromString(box.textColor), LEFT, SHADOW, FlxColor.fromString(box.shadowTextColor), false);
-		dialogueText.antialiasing = box.antialiasing;
-		dialogueText.borderSize = box.shadowWidth;
+
+		/*dialogueText = new FlxTypeText(box.textOffsets[0], box.textOffsets[1], box.textWidth, '', 32);
+			dialogueText.setFormat(Paths.font(box.font), 32, FlxColor.fromString(box.textColor), LEFT, SHADOW, FlxColor.fromString(box.shadowTextColor), false);
+			dialogueText.antialiasing = box.antialiasing;
+			dialogueText.borderSize = box.shadowWidth;
+			add(dialogueText);
+			dialogueText.completeCallback = finishLine; */
+		dialogueText = new Textbox(box.textOffsets[0], box.textOffsets[1], {
+			textFieldWidth: box.textWidth,
+			fontSize: 32,
+			font: Paths.font(box.font),
+			color: FlxColor.fromString(box.textColor),
+		});
+		dialogueText.statusChangeCallbacks.push(
+			(status) -> {
+				if (status == DONE) {
+					finishLine();
+				}
+				else if(status == FULL){
+					dialogueText.continueWriting();
+				}
+			}
+		);
+		dialogueText.characterDisplayCallbacks.push((text) -> {
+			text.antialiasing = box.antialiasing;
+			text.borderStyle = SHADOW;
+			text.alignment = LEFT;
+			text.borderSize = box.shadowWidth;
+			text.borderColor = FlxColor.fromString(box.shadowTextColor);
+			if(sounds.length > 0){
+				FlxG.random.getObject(sounds).play();
+			}
+		});
 		add(dialogueText);
-		dialogueText.completeCallback = finishLine;
 
 		new FlxTimer().start(introDelay, function(tmr:FlxTimer) {
 			canProgressDialogue = true;
@@ -77,21 +105,20 @@ class DialogueCutscene extends Cutscene
 		this.cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
 	}
 
-	public function finishLine()
+	public function finishLine() 
 	{
 		finishedLine = true;
 		box.script?.call("onFinishLineDialogue");
 	}
 
-	function loadCharacters()		
-	{
+	function loadCharacters() {
 		var characterMap:Map<String, Bool> = new Map<String, Bool>();
-		//Does a loop through the entire dialogue file and checks for characters
+		// Does a loop through the entire dialogue file and checks for characters
 		for (i in 0...dialogueFile.dialogue.length) {
-			if(dialogueFile.dialogue[i] != null && dialogueFile.dialogue[i].character != null) {
+			if (dialogueFile.dialogue[i] != null && dialogueFile.dialogue[i].character != null) {
 				var newCharacter:String = dialogueFile.dialogue[i].character;
-				if(!characterMap.exists(newCharacter) || !characterMap.get(newCharacter)) 
-					characterMap.set(newCharacter, true);//adds to the map only if the character doesnt already exist
+				if (!characterMap.exists(newCharacter) || !characterMap.get(newCharacter))
+					characterMap.set(newCharacter, true); // adds to the map only if the character doesnt already exist
 			}
 		}
 
@@ -105,45 +132,40 @@ class DialogueCutscene extends Cutscene
 		}
 	}
 
-	override function update(elapsed:Float)
+	override function update(elapsed:Float) 
 	{
-	   	if (FlxG.keys.justPressed.SPACE && canProgressDialogue)
+		if (FlxG.keys.justPressed.SPACE && canProgressDialogue) 
 		{
-			if(finishedLine)
+			if (finishedLine) 
 			{
 				curLine++;
 				createNewLine();
-			}
-			else
+			} else 
 			{
 				skipCurLine();
 			}
-
 		}
-		//todo: maybe have a proper log book for dialogue.
-		
+		// todo: maybe have a proper log book for dialogue.
+
 		super.update(elapsed);
 	}
 
 	/**
 	 * Function thats called when a line of dialogue is skipped mid sentence.
 	 */
-	private function skipCurLine()
-	{
+	private function skipCurLine() {
 		box?.script?.call("onSkipLine");
-		dialogueText.skip();
+		//dialogueText.skip();
 	}
 
 	/**
 	 * Creates a new line of dialogue.
 	 */
-	public function createNewLine()
-	{
+	public function createNewLine() {
 		FlxG.sound.play(Paths.sound(box.dialoguePressedSound), 0.7);
 		box?.script?.call("onNewLine");
-		
-		if(curLine >= dialogueFile.dialogue.length)
-		{
+
+		if (curLine >= dialogueFile.dialogue.length) {
 			onEnd.dispatch(false);
 			return;
 		}
@@ -155,37 +177,36 @@ class DialogueCutscene extends Cutscene
 
 		var curCharcter:Int = 0;
 		for (i in 0...characters.length) {
-			if(characters[i].curChar == curDialogueLine.character) {
+			if (characters[i].curChar == curDialogueLine.character) {
 				curCharcter = i;
 				break;
 			}
 		}
-		if(!keepAllCharactersOnScreen)
-		for (i in 0...characters.length) {
-			characters[i].alpha = 0;
-			if(characters[i] ==  characters[curCharcter]) {
-				characters[curCharcter].alpha = 1;
+		if (!keepAllCharactersOnScreen)
+			for (i in 0...characters.length) {
+				characters[i].alpha = 0;
+				if (characters[i] == characters[curCharcter]) {
+					characters[curCharcter].alpha = 1;
+				}
 			}
-		}
 		curCharacter = characters[curCharcter];
 		curCharacter.animation.play(curDialogueLine.character_anim);
 		getTextSound();
-		dialogueText.size = getTextSize(curDialogueLine.text_size);
-			   
-		dialogueText.resetText(curDialogueLine.text);
-		dialogueText.start(curDialogueLine.text_speed);
+		@:privateAccess
+		dialogueText.settings.fontSize = getTextSize(curDialogueLine.text_size);
+		dialogueText.setText(curDialogueLine.text);
+		dialogueText.startWriting();
+		dialogueText.bring();
 	}
 
 	/**
 	 * Function that's only called when dialogue is ending.
 	 * @param wasSkipped 
 	 */
-	function endDialogue(wasSkipped:Bool)
-	{
+	function endDialogue(wasSkipped:Bool) {
 		box?.script?.call("onDialogueEnded");
 
-		new FlxTimer().start(1, function(tmr:FlxTimer)
-		{
+		new FlxTimer().start(1, function(tmr:FlxTimer) {
 			destroy();
 		});
 	}
@@ -196,10 +217,10 @@ class DialogueCutscene extends Cutscene
 	 * @param _lineTextSize 
 	 * @return Int
 	 */
-	inline function getTextSize(_lineTextSize:Int):Int 
+	inline function getTextSize(_lineTextSize:Int):Int
 		return _lineTextSize > 0 ? _lineTextSize : box.textSize;
 
-	override public function restart(){
+	override public function restart() {
 		curLine = 0;
 		createNewLine();
 	}
@@ -208,27 +229,28 @@ class DialogueCutscene extends Cutscene
 	 * Function thats called to retrieve text sound.
 	 * Leaving all fields empty, will not play a sound.
 	 */
-	private function getTextSound()
-	{
+	private function getTextSound() {
 		var dialogueTalkSound:Array<String> = null;
-		if(dialogueFile.dialogue[curLine].line_sound != null)
+		if (dialogueFile.dialogue[curLine].line_sound != null)
 			dialogueTalkSound = dialogueFile.dialogue[curLine].line_sound;
-		else if(curCharacter.talkSound != null)
+		else if (curCharacter.talkSound != null)
 			dialogueTalkSound = curCharacter.talkSound;
-		else if(box.dialogueTalkSound != null)
+		else if (box.dialogueTalkSound != null)
 			dialogueTalkSound = box.dialogueTalkSound;
 
-		if(dialogueTalkSound != null)
-		dialogueText.sounds = [for (dialogueSound in dialogueTalkSound) FlxG.sound.load(Paths.sound(dialogueSound), 0.6)];
+		if (dialogueTalkSound != null)
+			sounds = [
+				for (dialogueSound in dialogueTalkSound) FlxG.sound.load(Paths.sound(dialogueSound), 0.6)
+			];
 	}
 
 	/**
 	 * Play dialogue box animation
 	 * @param _anim 
 	 */
-	function playBoxAnimation(_anim:String)
-	{
-		if(_anim == null) return;
+	function playBoxAnimation(_anim:String) {
+		if (_anim == null)
+			return;
 		box?.animation.play(_anim);
 	}
 }
