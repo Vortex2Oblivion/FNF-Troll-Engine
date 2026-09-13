13d6ca70c82f0a65b42d63a0a84387719639cf19
riconuts <68708956+riconuts@users.noreply.github.com>
2026-09-13 19:34:26 -0300

    Update MainMenuState.hx

--- source/funkin/states/MainMenuState.hx
+++ source/funkin/states/MainMenuState.hx
@@ -106,7 +106,7 @@ class MainMenuState extends MusicBeatState
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
 		
-		changeItem();
+		changeSelection();
 
 		super.create();
 
